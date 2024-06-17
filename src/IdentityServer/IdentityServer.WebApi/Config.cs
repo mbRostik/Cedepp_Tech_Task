fc68248e742e238e8a5d1afac163ee28d405571c
mbRostik <rost.daskalyuk@gmail.com>
2024-06-17 16:07:25 +0300

    Identity server template was user and configured for the current project Migrations for the DB were created and applied

---
+++ src/IdentityServer/IdentityServer.WebApi/Config.cs
@@ -0,0 +1,52 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer.WebApi
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+           new IdentityResource[]
+           {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource("roles", new[] { "role" })
+           };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+           new ApiScope[]
+           {
+               new ApiScope("Cedepp.WebApi.Scope")
+
+           };
+
+        public static IEnumerable<ApiResource> ApiResources => new[] {
+            new ApiResource("Cedepp.WebApi")
+             {
+                 Scopes=new List<string>{ "Cedepp.WebApi.Scope"},
+                 ApiSecrets=new List<Secret>{new Secret("Cedepp.WebApi.Secret".Sha256())},
+                 UserClaims = new List<string> { "role" }
+             }
+        };
+
+        public static IEnumerable<Client> Clients =>
+           new Client[]
+           {
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = {new Secret("OnlyUserKnowsThisSecret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:5173/signin-oidc" },
+                    FrontChannelLogoutUri="https://localhost:5173/signout-oidc",
+                    PostLogoutRedirectUris={ "https://localhost:5173/signout-callback-oidc" },
+                    AllowOfflineAccess = true,
+                    AllowedScopes =
+                    {"openid", "profile", "Cedepp.WebApi.Scope"},
+                    RequireConsent = true,
+                    RequirePkce=true,
+                    AllowPlainTextPkce=true,
+                    AllowedCorsOrigins = { "https://localhost:5173" }
+                }
+           };
+    }
+}
--- src/IdentityServer/IdentityServer.WebApi/Program.cs
+++ src/IdentityServer/IdentityServer.WebApi/Program.cs
@@ -1,25 +1,161 @@
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using IdentityServer.Infrastructure.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using RabbitMQ.Client;
+using System.Net;
+using MessageBus.Messages;
+using IdentityServer.WebApi;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var assembly = typeof(Program).Assembly.GetName().Name;
+var assembly2 = typeof(IdentityServerDbContext).Assembly.GetName().Name;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var defaultConnString = builder.Configuration.GetConnectionString("MSSQLConnection");
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
+builder.Services.AddRazorPages().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+builder.Services.AddControllersWithViews().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("uk-UA"),
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedUICultures = supportedCultures;
+    options.SupportedCultures = supportedCultures;
+
+    options.RequestCultureProviders.Insert(0, new CookieRequestCultureProvider()
+    {
+        CookieName = CookieRequestCultureProvider.DefaultCookieName
+    });
+});
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+        {
+            IndexFormat = $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfReplicas = 1,
+            NumberOfShards = 2
+        })
+        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+        .ReadFrom.Configuration(context.Configuration);
+});
+
+
+builder.Services.AddDbContext<IdentityServerDbContext>(options =>
+{
+    options.UseSqlServer(defaultConnString);
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => { options.SignIn.RequireConfirmedAccount = true; options.User.AllowedUserNameCharacters += " "; })
+    .AddEntityFrameworkStores<IdentityServerDbContext>()
+     .AddDefaultTokenProviders();
+
+
+
+builder.Services.AddDataProtection()
+    .SetApplicationName("IdentityServer.WebApi")
+    .PersistKeysToDbContext<IdentityServerDbContext>();
+
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddConfigurationStore<ConfigurationDbContext>(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(defaultConnString,
+            sql => sql.MigrationsAssembly(assembly2));
+    })
+    .AddOperationalStore<PersistedGrantDbContext>(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(defaultConnString,
+            sql => sql.MigrationsAssembly(assembly2));
+    })
+    .AddAspNetIdentity<IdentityUser>()
+    .AddDeveloperSigningCredential();
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((cxt, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.Publish<UserCreationEvent>(p => p.ExchangeType = ExchangeType.Fanout);
+
+    });
+
+});
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.SignInScheme = Duende.IdentityServer.IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+        var googleAuth = builder.Configuration.GetSection("Authentication:Google");
+
+        options.ClientId = googleAuth["ClientId"];
+        options.ClientSecret = googleAuth["ClientSecret"];
+
+    });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
+app.UseRequestLocalization();
+app.UseIdentityServer();
 app.UseAuthorization();
+app.MapRazorPages().RequireAuthorization();
 
-app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+SeedData.EnsureSeedData(app);
 
 app.Run();
+
